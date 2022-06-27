bbabbc5fd6621fe25def6cdc97fad678e6b9032c
fasha-20 <fashareo17@gmail.com>
2022-06-27 09:38:13 +0700

    Perubahan

--- ci4/app/Views/about.php
+++ ci4/app/Views/about.php
@@ -5,10 +5,11 @@
     <title><?= $title; ?></title>
 </head>
 <body>
-    
+
     <?= $this->include('template/header'); ?>
     <h1><?= $title; ?></h1>
     <hr>
+    
     <p><?= $content; ?></p>
     <?= $this->include('template/footer'); ?>
 </body>
